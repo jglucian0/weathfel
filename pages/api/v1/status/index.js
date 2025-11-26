e83ece777592694a8f5ac46f2bb7cb5acbde28be
João Gabriel Luciano <jglucianoi@icloud.com>
2025-11-25 23:43:44 -0300

    feat: adds `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,8 @@
-import database from "../../../../infra/databse.js"
+import database from "infra/databse.js"
 
 async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;')
+  console.log(result.rows)
   response.status(200).json({ response: "200" })
 }
 
